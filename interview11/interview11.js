9362a3abaa5eaa5f21368611fad23f12a399868d
Sumaiyah Lee <sumaiyahlee.sl@gmail.com>
2020-04-16 18:57:34 -0500

    simplifying for loop

--- interview11/interview11.js
+++ interview11/interview11.js
@@ -3,17 +3,18 @@
  * @return {string}
  */
 var frequencySort = function(s) {
-    let sArray = s.split('');
-    let frequency = {};
-    for (let i = 0; i < sArray.length; i++) {
-        const ch = sArray[i];
-        if (frequency[ch]) {
-            frequency[ch]++;
-        }
-        else {
-            frequency[ch] = 1;
-        }
-    }
+    // let sArray = s.split('');
+    const frequency = {};
+    // for (let i = 0; i < sArray.length; i++) {
+    //     const ch = sArray[i];
+    //     if (frequency[ch]) {
+    //         frequency[ch]++;
+    //     }
+    //     else {
+    //         frequency[ch] = 1;
+    //     }
+    // }
+    s.split('').forEach(ch => frequency[ch] = frequency[ch] + 1 || 1);
 
     let fLetters = Object.entries(frequency);
     const sortArray = fLetters.sort((a,b) => a[1] - b[1]).map((arr) => arr[0]).reverse().join('');
