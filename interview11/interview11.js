b7d1e0ec3bfc437bb88603a458d71c3f880ed10d
Sumaiyah Lee <sumaiyahlee.sl@gmail.com>
2020-04-16 18:49:55 -0500

    adding interview problem #11

---
+++ interview11/interview11.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    let sArray = s.split('');
+    let frequency = {};
+    for (let i = 0; i < sArray.length; i++) {
+        const ch = sArray[i];
+        if (frequency[ch]) {
+            frequency[ch]++;
+        }
+        else {
+            frequency[ch] = 1;
+        }
+    }
+
+    let fLetters = Object.entries(frequency);
+    const sortArray = fLetters.sort((a,b) => a[1] - b[1]).map((arr) => arr[0]).reverse().join('');
+    return frequency;
+};
+
+
+console.log(frequencySort("treeee"));
